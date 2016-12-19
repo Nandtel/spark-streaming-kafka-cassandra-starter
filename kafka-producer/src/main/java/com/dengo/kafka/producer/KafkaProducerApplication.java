2ad9e2f6d97e30dd5a3ca8bb9dfd3e95f61f931c
Nandtel <Nandtel@outlook.com>
2016-12-19 11:19:35 +0300

    Refactor the code of kafka-producer module

--- kafka-producer/src/main/java/com/dengo/kafka/producer/KafkaProducerApplication.java
+++ kafka-producer/src/main/java/com/dengo/kafka/producer/KafkaProducerApplication.java
@@ -13,50 +13,28 @@ import java.util.Properties;
 import java.util.Random;
 import java.util.UUID;
 
-/**
- * @author Sem Babenko
- */
 public class KafkaProducerApplication {
 
     public static void main(String[] args) {
 
         String[] words = new String[]{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
 
-
         Random ran = new Random(System.currentTimeMillis());
-
-        //Assign topicName to string variable
         String topicName = "Hello-Kafka";
 
-        // create instance for properties to access producer configs
         Properties props = new Properties();
-
-        //Assign localhost id
         props.put("bootstrap.servers", "localhost:9092");
-
-        //Set acknowledgements for producer requests.
         props.put("acks", "all");
-
-        //If the request fails, the producer can automatically retry,
         props.put("retries", 0);
-
-        //Specify buffer size in config
         props.put("batch.size", 16384);
-
-        //Reduce the no of requests less than 0
         props.put("linger.ms", 1);
-
-        //The buffer.memory controls the total amount of memory available to the producer for buffering.
         props.put("buffer.memory", 33554432);
-
         props.put("key.serializer", StringSerializer.class);
-
         props.put("value.serializer", StringSerializer.class);
 
         Producer<String, String> producer = new KafkaProducer<>(props);
         Gson gson = new Gson();
         try {
-
             while (true) {
 
                 producer.send(new ProducerRecord<>(topicName, gson.toJson(initNewEvent(words[ran.nextInt(words.length)]))));
--- model-common/src/main/scala/com/dengo/model/common/Event.scala
+++ model-common/src/main/scala/com/dengo/model/common/Event.scala
@@ -1,7 +1,4 @@
 package com.dengo.model.common
 
-/**
-  * @author Sem Babenko
-  */
 class Event(var id: String, var name: String)
   extends Serializable {}
